e892d0830bdb7786f074c0f28307241655eac263
Daniil Gladkikh <gladkikhdan@gmail.com>
2021-04-16 15:12:08 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = (props) => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <div className="content-wrapper">
-        <Navbar />
-        <div className="main-content"> { /*profile, dialogs, news, music */}
-          {/* <Profile /> */}
-          <Dialogs />
-        </div>        
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <div className="content-wrapper">
+          <Navbar />
+          <div className="main-content"> { /*profile, dialogs, news, music */}
+            <Route path='/profile' component={Profile} />
+            <Route path='/dialogs' component={Dialogs} />
+            <Route path='/news' component={News} />
+            <Route path='/music' component={Music} />
+            <Route path='/settings' component={Settings} />
+          </div>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import css from './Navbar.module.css';
 
 const Navbar = () => {
     return (
     <nav className={css.nav}>
-        <div className={css.item}><a>Profile</a></div>
-        <div className={css.item}><a>Messages</a></div>
-        <div className={css.item}><a>News</a></div>
-        <div className={css.item}><a>Music</a></div>
-        <div className={css.item}><a>Settings</a></div>
+        <div className={css.item}>
+            <NavLink to="/profile" activeClassName={css.active}>Profile</NavLink>
+        </div>
+        <div className={css.item}>
+            <NavLink to="/dialogs" activeClassName={css.active}>Dialogs</NavLink>
+        </div>
+        <div className={css.item}>
+            <NavLink to="/news" activeClassName={css.active}>News</NavLink>
+        </div>
+        <div className={css.item}>
+            <NavLink to="/music" activeClassName={css.active}>Music</NavLink>
+        </div>
+        <div className={css.item}>
+            <NavLink to="/settings" activeClassName={css.active}>Settings</NavLink>
+        </div>
     </nav>
     );
 }
