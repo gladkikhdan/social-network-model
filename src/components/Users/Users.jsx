8b864c8f5170177d17f46ba6e5292ea9429a1ed1
Daniil Gladkikh <gladkikhdan@gmail.com>
2021-04-25 12:14:21 +0300

    add

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,61 +1,68 @@
+import axios from 'axios';
 import React from 'react';
 import css from './Users.module.css';
 
+
 let Users = (props) => {
     if (props.users.length === 0) {
-        debugger;
-        props.setUsers([
-            {
-                id: 1,
-                avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
-                isFollowed: false,
-                name: 'Sam',
-                status: 'Listening drake RN',
-                location: {
-                    city: 'Minsk', country: 'Belarus',
-                },
-            },
-            {
-                id: 2,
-                avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
-                isFollowed: false,
-                name: 'Alex',
-                status: 'I love cheesecake',
-                location: {
-                    city: 'Moscow', country: 'Russia',
-                }
-            },
-            {
-                id: 3,
-                avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
-                isFollowed: true,
-                name: 'Elena',
-                status: 'Learning javascript',
-                location: {
-                    city: 'Saint P', country: 'Russia',
-                }
-            },
-            {
-                id: 4,
-                avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
-                isFollowed: false,
-                name: 'Richard',
-                status: 'smoking loud',
-                location: {
-                    city: 'Atlanta', country: 'USA',
-                }
-            },
-            {
-                id: 5,
-                avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
-                isFollowed: true,
-                name: 'Grinch',
-                status: 'Hating Santa!',
-                location: {
-                    city: 'Middle of nowhere', country: 'North pole',
-                }
-            },
-        ]);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            debugger;
+            props.setUsers(response.data.items);
+        });
+
+        
+        // props.setUsers([
+        //     {
+        //         id: 1,
+        //         avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
+        //         isFollowed: false,
+        //         name: 'Sam',
+        //         status: 'Listening drake RN',
+        //         location: {
+        //             city: 'Minsk', country: 'Belarus',
+        //         },
+        //     },
+        //     {
+        //         id: 2,
+        //         avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
+        //         isFollowed: false,
+        //         name: 'Alex',
+        //         status: 'I love cheesecake',
+        //         location: {
+        //             city: 'Moscow', country: 'Russia',
+        //         }
+        //     },
+        //     {
+        //         id: 3,
+        //         avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
+        //         isFollowed: true,
+        //         name: 'Elena',
+        //         status: 'Learning javascript',
+        //         location: {
+        //             city: 'Saint P', country: 'Russia',
+        //         }
+        //     },
+        //     {
+        //         id: 4,
+        //         avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
+        //         isFollowed: false,
+        //         name: 'Richard',
+        //         status: 'smoking loud',
+        //         location: {
+        //             city: 'Atlanta', country: 'USA',
+        //         }
+        //     },
+        //     {
+        //         id: 5,
+        //         avatarUrl: "https://cdnimg.rg.ru/i/gallery/84f24d10/19_b6265e7a.jpg",
+        //         isFollowed: true,
+        //         name: 'Grinch',
+        //         status: 'Hating Santa!',
+        //         location: {
+        //             city: 'Middle of nowhere', country: 'North pole',
+        //         }
+        //     },
+        // ]);
     }
 
     let users = props.users.map(user => (
