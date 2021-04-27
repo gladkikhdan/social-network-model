fd150b138764f3c3d474baa75282156c5a9f326b
Daniil Gladkikh <gladkikhdan@gmail.com>
2021-04-27 13:33:27 +0300

    MVP, react/redux

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,41 +4,37 @@ import css from './Users.module.css';
 import userPhoto from '../../assets/images/user.jpg';
 
 
-let Users = (props) => {
-    let getUsers = () => {
-        if (props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                debugger;
-                props.setUsers(response.data.items);
-            });
-        }
+class Users extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
     }
 
-    let users = props.users.map(user => (
-        <div key={user.id} className={css.user}>
-            <div className={css.follow}>
-                {
-                    user.followed
-                        ? <button onClick={() => { props.unFollow(user.id); }}>UNFOLLOW</button>
-                        : <button onClick={() => { props.follow(user.id); }}>FOLLOW</button>
-                }
-            </div>
-            <div className={css.avatar}>
-                <img src={user.photos.small != null ? user.photos.small : userPhoto} />
-            </div>
-            <div className={css.description}>
-                description, name: {user.name}
-            </div>
-        </div>));
+    render() {
+        let users = this.props.users.map(user => (
+            <div key={user.id} className={css.user}>
+                <div className={css.follow}>
+                    {
+                        user.followed
+                            ? <button onClick={() => { this.props.unFollow(user.id); }}>UNFOLLOW</button>
+                            : <button onClick={() => { this.props.follow(user.id); }}>FOLLOW</button>
+                    }
+                </div>
+                <div className={css.avatar}>
+                    <img src={user.photos.small != null ? user.photos.small : userPhoto} />
+                </div>
+                <div className={css.description}>
+                    description, name: {user.name}
+                </div>
+            </div>));
 
-    return (
-        <div className={css.wrapper}>
-            <div>
-                <button onClick={getUsers}>Get Users</button>
+        return (
+            <div className={css.wrapper}>
+                {users}
             </div>
-            {users}
-        </div>
-    );
+        );
+    }
 }
 
 export default Users;
